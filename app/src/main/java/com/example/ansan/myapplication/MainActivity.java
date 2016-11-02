c9c93defc10a58226974c7ed3beaa206f4657fa6
Gosemin <semingo@naver.com>
2016-11-02 12:31:10 +0900

    안드로이드 SD Card 생성 권한

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ansan.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public  void onClick(View v){
+        switch (v.getId()){
+            case R.id.button:   //폴더생성
+                break;
+            case R.id.button2:  //폴더 삭제
+                break;
+            case R.id.button3:  //파일생성
+                break;
+            case R.id.button4:  //파일읽기
+                break;
+            case R.id.button5:  //파일 목록 가져오기
+                break;
+
+        }
     }
 }
